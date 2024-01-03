a3cf46b41e8aef35ad6fafd30f3c1165a7948df6
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2024-01-03 16:49:44 -0500

    sutfff

--- src/main/java/team1403/robot/RobotContainer.java
+++ src/main/java/team1403/robot/RobotContainer.java
@@ -2,11 +2,15 @@ package team1403.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import team1403.lib.util.CougarLogger;
 import team1403.robot.swerve.SwerveCommand;
 import team1403.robot.swerve.SwerveSubsystem;
 import team1403.robot.turret.Limelight;
 import team1403.robot.turret.Turret;
+
+import java.time.Instant;
 
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.XboxController;
@@ -65,6 +69,22 @@ public class RobotContainer {
             () -> deadband(driveController.getRightX(), 0),
             () -> driveController.getYButton(),
             () -> driveController.getRightTriggerAxis()));
+
+            new Trigger(() -> driveController.getAButton())
+            .onTrue(new InstantCommand(() -> m_swerveSubsystem.zeroGyroscope(), m_swerveSubsystem)); 
+
+            new Trigger(() -> driveController.getXButton())
+            .whileTrue(new InstantCommand(() -> m_swerveSubsystem.setXModeEnabled(true), m_swerveSubsystem));
+
+            new Trigger(() -> driveController.getBButton())
+            .onTrue(new InstantCommand(() -> m_Turret.centerPoint(), m_Turret));
+            new Trigger(() -> driveController.getPOV() == 270)
+            .onTrue(new InstantCommand(() -> m_Turret.setSpeed(0.2), m_Turret))
+            .onFalse(new InstantCommand(() -> m_Turret.setSpeed(0), m_Turret));
+            new Trigger(() -> driveController.getPOV() == 90)
+            .onTrue(new InstantCommand(() -> m_Turret.setSpeed(-0.2), m_Turret))
+            .onFalse(new InstantCommand(() -> m_Turret.setSpeed(0), m_Turret));
+
       }
       private double deadband(double value, double deadband) {
         if (Math.abs(value) > deadband) {
